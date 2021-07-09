f2cc439ad441b7d724bbd20da4762035e57a85d3
abdellah koris <korabdo2010@gmail.com>
2021-07-09 02:52:14 +0200

    version1

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
         const {error , user } = addUser({id : socket.id , name,room} );
 
         socket.emit('message',{user : 'admin' , text : `${user.name},welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message',{user : 'admin' , text : `${user.name},has joined ! `});
+        socket.broadcast.to(user.room).emit('message',{user : 'admin' , text : `${user.name},has joined now  ! `});
 
         if(error) return callback(error);
         socket.join(user.room); 
